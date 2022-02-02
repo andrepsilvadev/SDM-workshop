5fe2e64ed78d155f8ce458033402bafeb5d7fcfb
Andre Silva <pintodasilva.a@gmail.com>
2022-02-02 20:13:19 +0100

    added pseudoabsence script

--- scripts/biomod.R
+++ scripts/biomod.R
@@ -14,26 +14,10 @@ head(myRespCoord)
 nrow(myRespCoord)
 
 # Initialisation
-myBiomodData_PArandom <- BIOMOD_FormatingData(resp.var = myResp,
+myBiomodData <- BIOMOD_FormatingData(resp.var = myResp,
                                      expl.var = env_vars,
                                      resp.xy = myRespCoord,
                                      resp.name = myRespName)
-
-myBiomodData_PAdisk <-BIOMOD_FormatingData(resp.var,
-                                    expl.var,
-                                    resp.xy = NULL,
-                                    resp.name = NULL,
-                                    eval.resp.var = NULL,
-                                    eval.expl.var = NULL,
-                                    eval.resp.xy = NULL,
-                                    PA.nb.rep = 0,
-                                    PA.nb.absences = 1000,
-                                    PA.strategy =c('random','disk'),
-                                    PA.dist.min = 3000,
-                                    PA.dist.max = 10000,
-                                    PA.sre.quant = NULL,
-                                    PA.table = NULL,
-                                    na.rm = TRUE)
 
 #myBiomodOption <- BIOMOD_ModelingOptions(
 #  MAXENT.Phillips = list(path_to_maxent.jar = getwd())) # to later add maxent.jar path
@@ -57,37 +41,26 @@ myBiomodEM <- BIOMOD_EnsembleModeling(
   chosen.models = 'all',
   em.by='all',
   eval.metric = c('ROC'),
-  eval.metric.quality.threshold = c(0.9),
+  eval.metric.quality.threshold = c(0.8),
   prob.mean = TRUE, # estimate the mean of probabilities
   prob.cv = TRUE, # coefficient variation
   prob.ci = TRUE, # confidence interval
   prob.ci.alpha = 0.05, # significance level for the confidence interval
   prob.median = TRUE, # estimate the median of probabilities
-  prob.mean.weight = FALSE, # estimate the weighted mean of probabilities
+  prob.mean.weight = TRUE, # estimate the weighted mean of probabilities
   prob.mean.weight.decay = 'proportional') # bases weights on the model evaluation scores
 
 # project models in current environment
 myBiomodCurrent <- BIOMOD_Projection(modeling.output = myBiomodModelOut,
-                                      new.env = env_vars, 
-                                      proj.name = "current",
-                                      selected.models = 'all',
-                                      binary.meth = 'TSS')
+                                     new.env = env_vars,
+                                     proj.name = "current",
+                                     selected.models = 'all',
+                                     binary.meth = 'TSS')
 #plot(myBiomodCurrent)
 
 # ensemble-models projections on current environment
 myBiomodEFCurrent <- BIOMOD_EnsembleForecasting(EM.output = myBiomodEM,
                                                 projection.output = myBiomodCurrent)
-
-BIOMOD_EnsembleForecasting( EM.output,
-                            projection.output = NULL,
-                            new.env = NULL,
-                            xy.new.env = NULL,
-                            selected.models ='all',
-                            proj.name = NULL,
-                            binary.meth = NULL,
-                            filtered.meth = NULL,
-                            compress = TRUE,...)
-
 plot(myBiomodEFCurrent)
 
 # project models in future scenario Rcp26_70
@@ -96,14 +69,12 @@ myBiomodRcp26_70 <- BIOMOD_Projection(modeling.output = myBiomodModelOut,
                                      proj.name = "bio_Rcp26_70",
                                      selected.models = 'all',
                                      binary.meth = 'TSS')
-
 plot(myBiomodRcp26_70)
+
 myBiomodEFRcp26_70 <- BIOMOD_EnsembleForecasting(
   EM.output = myBiomodEM,
   projection.output = myBiomodRcp26_70)
 plot(myBiomodEFRcp26_70)
-
-
 
 # project models in future scenario Rcp26_70
 myBiomodRcp85_70 <- BIOMOD_Projection(modeling.output = myBiomodModelOut,
@@ -111,24 +82,22 @@ myBiomodRcp85_70 <- BIOMOD_Projection(modeling.output = myBiomodModelOut,
                                       proj.name = "bio_Rcp85_70",
                                       selected.models = 'all',
                                       binary.meth = 'TSS')
-
 plot(myBiomodRcp85_70)
+
 myBiomodEFRcp85_70 <- BIOMOD_EnsembleForecasting(
   EM.output = myBiomodEM,
   projection.output = myBiomodRcp85_70)
 plot(myBiomodEFRcp85_70)
 
 
-
-
-## Model evaluation
-# get AUC and TSS
+# Model evaluation
+## get AUC and TSS
 get_evaluations(myBiomodModelOut)
 
-# see what variables influence more AUC
+## see what variables influence more AUC
 get_variables_importance(myBiomodModelOut)
 
-# species relationship with environmental variables
+## species relationship with environmental variables
 response.plot2(models = BIOMOD_LoadModels(myBiomodModelOut, models='GLM'),
                Data = get_formal_data(myBiomodModelOut,'expl.var'),
                show.variables= get_formal_data(myBiomodModelOut,'expl.var.names'),
@@ -140,16 +109,34 @@ response.plot2(models = BIOMOD_LoadModels(myBiomodModelOut, models='GLM'),
 
 
 
+# plot ensemble forecasts
+## load models manually 
+list_projCurrent <- list.files("Prionailurus.bengalensis/proj_current/individual_projections", full.names = TRUE)
+(list_proj <- grep("grd", list_proj, value=TRUE))
+Pbcurrent <- raster::stack(list_proj)
+names(Pbcurrent) <- grep("grd", list_proj, value=TRUE)
+plot(Pbcurrent)
 
+list_Rcp26_70  <- list.files("Prionailurus.bengalensis/proj_bio_Rcp26_70/individual_projections", full.names = TRUE)
+(list_proj <- grep("grd", list_Rcp26_70, value=TRUE))
+PbRcp26_70 <- raster::stack(list_proj)
+names(PbRcp26_70) <- grep("grd", list_proj, value=TRUE)
+plot(PbRcp26_70)
 
+list_Rcp85_70  <- list.files("Prionailurus.bengalensis/proj_bio_Rcp85_70/individual_projections", full.names = TRUE)
+(list_proj <- grep("grd", list_Rcp85_70, value=TRUE))
+PbRcp85_70 <- raster::stack(list_proj)
+names(PbRcp85_70) <- grep("grd", list_proj, value=TRUE)
+plot(PbRcp85_70)
 
+## join mean predictions and cv for the different future climatic scenarios in raster stack
+Pbforecast <- raster::stack(Pbcurrent[[2]],
+                            Pbcurrent[[1]],
+                            PbRcp26_70[[2]],
+                            PbRcp85_70[[2]])
+names(Pbforecast) <- c("Pb_suitability_current",
+                       "Pb_suitability_CV",
+                       "Pb_suitability_Rcp26_70",
+                       "Pb_suitability_Rcp85_70")
+plot(Pbforecast) # typical final output map for your paper
 
-## plot ensemble forecasts
-# load models manually 
-Pbcurrent <- raster("Prionailurus.bengalensis/proj_current/proj_current_Prionailurus.bengalensis_ensemble.grd")
-PbRcp26_70 <- raster("Prionailurus.bengalensis/proj_bio_Rcp26_70/proj_bio_Rcp26_70_Prionailurus.bengalensis_ensemble.grd")
-PbRcp85_70  <- raster("Prionailurus.bengalensis/proj_bio_Rcp85_70/proj_bio_Rcp85_70_Prionailurus.bengalensis_ensemble.grd")
-# join predictions from the different scenarios in raster stack
-Pbforecast <- raster::stack(Pbcurrent,PbRcp26_70,PbRcp85_70)
-names(forecast) <- c("Pb_Ensemble_current","Pb_Ensemble_Rcp26_70","Pb_Ensemble_Rcp85_70")
-plot(forecast)
